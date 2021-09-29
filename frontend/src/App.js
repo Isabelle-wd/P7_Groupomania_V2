a896de1bdd822795bf7781aaecf8287a22b8375d
isabelle-wd <isabelle.martinez.wd@gmail.com>
2021-09-29 16:28:51 +0200

    button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <div>
-      
-      <Navbar />
-    </div>
+    <>  
+      <Router>   
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -2,14 +2,38 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                <img
-                alt="logo"
-                src= "images/icon-left-font-monochrome-black.svg"
-              /> 
+                    <img
+                        alt="logo"
+                        src= "images/icon-left-font-monochrome-black.svg"
+                    />
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
+                    </div> 
+                    <ul className={click ? "nav-menu active" : "nav-menu" }>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/login" className="nav-links" onClick={closeMobileMenu}>
+                                Connexion
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/signup" className="nav-links" onClick={closeMobileMenu}>
+                                Inscription
+                            </Link>
+                        </li>
+                    </ul>
+                    
                 </div> 
             </nav>
 
